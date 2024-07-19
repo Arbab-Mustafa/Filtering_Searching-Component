5f94b39c6a8343168489d79d8ad933c19e6ad847
Arbab-Mustafa <22-SE-58@students.uettaxila.edu.pk>
2024-07-19 15:52:57 +0500

    Done again

--- server/webflowRoute.js
+++
@@ -1,9 +0,0 @@
-import { Router } from "express";
-import { getWebflowData } from "./webflowController.js";
-const route = Router();
-
-// add route for webflow api
-
-route.get("/", getWebflowData);
-
-export default route;
