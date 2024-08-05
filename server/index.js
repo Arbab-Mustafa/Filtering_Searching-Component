fbbeb245f175a41b1b1b3681dabf302bdb98a1c2
Arbab-Mustafa <22-SE-58@students.uettaxila.edu.pk>
2024-08-05 14:55:22 +0500

    Done...,.

--- server/index.js
+++ server/index.js
@@ -35,6 +35,9 @@ app.use("/", EventRouter);
 app.use("/", VenueRouter);
 app.use("/", GuestListRouter);
 app.use("/", EmailListRouter);
+app.get("/", (req, res) => {
+  res.send("Welcome to the API");
+});
 
 // app.use("/", webFlowRouter);
 
