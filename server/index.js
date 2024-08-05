0b48bd4f8f3f7ccd24fd696f4fa71e14bc9d0a18
Arbab-Mustafa <22-SE-58@students.uettaxila.edu.pk>
2024-08-06 01:29:09 +0500

    port

--- server/index.js
+++ server/index.js
@@ -13,7 +13,7 @@ import EmailListRouter from "./routes/EmailListRoute.js";
 dotenv.config();
 
 const app = express();
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3002;
 
 app.use(express.json());
 app.use(cors());
